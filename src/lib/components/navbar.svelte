5498da27a99dfcb4c03b2eacb93bc819e277c390
Spimy <officialspyme@gmail.com>
2023-07-30 22:02:59 +0400

    📦 NEW: Add functionality to theme toggler button

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
+	import type { SubmitFunction } from '@sveltejs/kit';
 
+	const submitUpdateTheme: SubmitFunction = ({ action, submitter }) => {
+		const theme = action.searchParams.get('theme') as Theme;
+		if (theme) {
+			document.documentElement.setAttribute('data-theme', theme);
+		}
+	};
+
+	export let data: { theme: Theme | undefined };
 	$: path = $page.url.pathname;
 </script>
 
@@ -11,11 +20,26 @@
 	</div>
 
 	<nav class="navbar__items">
-		<!-- WIP theme switcher ignore for now -->
-		<form method="POST" use:enhance>
-			<button aria-label="Toggle theme" type="submit" class="navbar__items__theme-toggler">
-				<img aria-hidden="true" src="icons/theme-toggle/light.svg" alt="theme-toggler-icon" />
-			</button>
+		<form method="POST" use:enhance={submitUpdateTheme}>
+			{#if !data.theme || data.theme === 'light'}
+				<button
+					aria-label="Toggle theme"
+					class="navbar__items__theme-toggler"
+					type="submit"
+					formaction="/?/setTheme&theme=dark&redirectTo={path}"
+				>
+					<img aria-hidden="true" src="icons/theme-toggle/to-dark.svg" alt="theme-toggler-icon" />
+				</button>
+			{:else}
+				<button
+					aria-label="Toggle theme"
+					class="navbar__items__theme-toggler"
+					type="submit"
+					formaction="/?/setTheme&theme=light&redirectTo={path}"
+				>
+					<img aria-hidden="true" src="icons/theme-toggle/to-light.svg" alt="theme-toggler-icon" />
+				</button>
+			{/if}
 		</form>
 
 		<input aria-label="Toggle menu" type="checkbox" name="burger" id="burger" />
