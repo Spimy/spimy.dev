c3ed18757e7ec98ca952a470b9eaa5bcee4e1def
Spimy <officialspyme@gmail.com>
2023-07-30 16:56:21 +0400

    📦 NEW: Add burger menu element

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -10,30 +10,35 @@
 		<a href="/"><img src="/logos/icon.png" alt="home-logo" /></a>
 	</div>
 
-	<nav class="navbar__menu-container">
+	<nav class="navbar__items">
 		<!-- WIP theme switcher ignore for now -->
 		<form method="POST" use:enhance>
-			<button type="submit" class="navbar__theme-switcher">Toggle Theme</button>
+			<button type="submit" class="navbar__items__theme-switcher">Toggle Theme</button>
 		</form>
 
-		<ul role="list" class="navbar__menu">
+		<input type="checkbox" name="burger" id="burger" />
+		<label for="burger"><img src="icons/burger.svg" alt="" /></label>
+
+		<ul role="list" class="navbar__items__menu">
 			<li>
-				<a href="/" aria-current={path === '/' ? 'page' : undefined} class="navbar__menu__item"
-					>Home</a
+				<a
+					href="/"
+					aria-current={path === '/' ? 'page' : undefined}
+					class="navbar__items__menu__item">Home</a
 				>
 			</li>
 			<li>
 				<a
 					href="/projects"
 					aria-current={path === '/projects' ? 'page' : undefined}
-					class="navbar__menu__item">Past Projects</a
+					class="navbar__items__menu__item">Past Projects</a
 				>
 			</li>
 			<li>
 				<a
 					href="/contact"
 					aria-current={path === '/contact' ? 'page' : undefined}
-					class="navbar__menu__item">Contact</a
+					class="navbar__items__menu__item">Contact</a
 				>
 			</li>
 		</ul>
