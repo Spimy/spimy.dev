bb4058adfb1030ffbebf084c2913e22dc30cb409
Spimy <officialspyme@gmail.com>
2023-07-30 22:08:28 +0400

    📦 NEW: Add hook to load theme from cookie

---
+++ src/hooks.server.ts
@@ -0,0 +1,20 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	let theme: Theme | null = null;
+
+	const newTheme = event.url.searchParams.get('theme') as Theme | null;
+	const cookieTheme = event.cookies.get('theme') as Theme | undefined;
+
+	if (newTheme) {
+		theme = newTheme;
+	} else if (cookieTheme) {
+		theme = cookieTheme;
+	}
+
+	if (!theme) return await resolve(event);
+
+	return resolve(event, {
+		transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
+	});
+};
