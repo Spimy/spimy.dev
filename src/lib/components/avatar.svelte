1097ed6dcc2307c170297f836459fa5205da96f8
Spimy <officialspyme@gmail.com>
2023-08-01 18:46:45 +0400

    📦 NEW: Add avatar component

---
+++ src/lib/components/avatar.svelte
@@ -0,0 +1,7 @@
+<div class="avatar">
+	<div class="avatar__corner avatar__corner--top-left" />
+	<div class="avatar__corner avatar__corner--bottom-left" />
+	<img src="/logos/character.png" alt="spimy's avatar" />
+	<div class="avatar__corner avatar__corner--top-right" />
+	<div class="avatar__corner avatar__corner--bottom-right" />
+</div>
