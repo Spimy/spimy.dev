4a65f2dd393dbf65487c9e4b27faf219e2ec0975
Spimy <officialspyme@gmail.com>
2023-08-06 20:33:52 +0400

    📦 NEW: Add noscript check

--- src/routes/(portfolio)/+layout.svelte
+++ src/routes/(portfolio)/+layout.svelte
@@ -8,6 +8,12 @@
 	$: ({ theme, currentYear } = data);
 </script>
 
+<noscript>
+	<input type="checkbox" id="close-noscript" name="close-noscript" class="btn btn--invert" />
+	<label for="close-noscript" class="btn btn--invert">X</label>
+	<div>Enable JavaScript for the best experience while using this portfolio.</div>
+</noscript>
+
 <Navbar {theme} />
 
 <main>
@@ -18,6 +24,35 @@
 
 <style lang="scss">
 	@use '../../scss/abstracts/mixins' as *;
+
+	noscript {
+		position: fixed;
+		bottom: 0;
+		text-align: center;
+		width: 100%;
+		z-index: 9999;
+
+		label {
+			cursor: pointer;
+			position: absolute;
+			right: 1rem;
+			top: 1rem;
+			padding: 0.1rem 0.4rem;
+		}
+
+		input {
+			display: none;
+
+			&:checked ~ * {
+				display: none;
+			}
+		}
+
+		div {
+			background-color: black;
+			padding: 3rem;
+		}
+	}
 
 	main {
 		padding-inline: var(--content-padding);
