f2d248bcbd76079ecefcc1d418697a39ed2dd7f3
Spimy <officialspyme@gmail.com>
2023-08-06 17:50:30 +0400

    📦 NEW: Add fontawesome solid icons

--- src/app.d.ts
+++ src/app.d.ts
@@ -13,4 +13,8 @@ declare module '@fortawesome/free-brands-svg-icons/index.es' {
 	export * from '@fortawesome/free-brands-svg-icons';
 }
 
+declare module '@fortawesome/free-solid-svg-icons/index.es' {
+	export * from '@fortawesome/free-solid-svg-icons';
+}
+
 export {};
