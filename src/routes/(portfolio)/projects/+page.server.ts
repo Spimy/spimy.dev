e58516956b0d39233dee5ea8037b262942a5f080
Spimy <officialspyme@gmail.com>
2023-08-07 22:42:25 +0400

    📦 NEW: Add ability to filter through projects by technology used

--- src/routes/(portfolio)/projects/+page.server.ts
+++ src/routes/(portfolio)/projects/+page.server.ts
@@ -1,3 +1,4 @@
+import { redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 
 export const load: PageServerLoad = async ({ fetch, url }) => {
@@ -7,6 +8,20 @@ export const load: PageServerLoad = async ({ fetch, url }) => {
 	let apiRoute = `/api/projects?page=${page}`;
 	if (technologies) apiRoute += `&technologies=${technologies}`;
 
-	const response: ProjectResponse = await fetch(apiRoute).then((response) => response.json());
-	return { ...response };
+	const projectResponse: ProjectResponse = await fetch(apiRoute).then((response) =>
+		response.json()
+	);
+	const technologyResponse: TechnologyResponse = await fetch('/api/technologies').then((response) =>
+		response.json()
+	);
+	return { ...projectResponse, ...technologyResponse };
+};
+
+export const actions: Actions = {
+	filterProjects: async ({ request }) => {
+		const form = await request.formData();
+		const technologies = form.getAll('technologies');
+		if (technologies.length === 0) throw redirect(302, `?page=1`);
+		throw redirect(302, `?page=1&technologies=${technologies}`);
+	}
 };
--- src/routes/(portfolio)/projects/+page.svelte
+++ src/routes/(portfolio)/projects/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { beforeNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import ProjectDisplay from '$lib/components/project-display.svelte';
@@ -7,12 +8,10 @@
 	import { fly } from 'svelte/transition';
 	import type { PageServerData } from './$types';
 
-	const technologies = $page.url.searchParams.get('technologies');
-	const technologiesParam = technologies ? `&technologies=${technologies}` : '';
-
 	const animationConstant = 200;
 	let inX: number;
 	let outX: number;
+	let checked: boolean = false;
 
 	beforeNavigate(({ from, to }) => {
 		const initialPage = Number(from?.url.searchParams.get('page') ?? 1);
@@ -28,72 +27,108 @@
 	});
 
 	export let data: PageServerData;
-	$: ({ projects, paginator } = data);
+	$: ({ projects, paginator, technologies } = data);
+	$: technologiesQuery = $page.url.searchParams.get('technologies');
+	$: technologiesParam = technologiesQuery ? `&technologies=${technologiesQuery}` : '';
 </script>
+
+<svelte:head>
+	<title>Spimy's Portfolio - Projects</title>
+</svelte:head>
 
 <section class="projects">
 	<h1>Projects</h1>
-	{#key $page.url}
-		<div class="projects__info">
-			<div
-				class="projects__info__grid"
-				in:fly={{ x: inX, duration: 300, delay: 300 }}
-				out:fly={{ x: outX, duration: 300 }}
-			>
-				{#each projects as project}
-					<ProjectDisplay {project} />
-				{/each}
-			</div>
-			{#if paginator.pageCount > 1}
-				<div class="projects__info__paginator">
-					{#if paginator.currentPage === 1}
-						<button aria-disabled="true" class="projects__info__paginator__item" disabled>
-							<i><Fa icon={faAngleLeft} /></i>
-						</button>
-					{:else}
-						<a
-							href="?page={paginator.currentPage === 1
-								? 1
-								: paginator.currentPage - 1}{technologiesParam}"
-							aria-disabled="false"
-							class="projects__info__paginator__item"
-						>
-							<i><Fa icon={faAngleLeft} /></i>
-						</a>
-					{/if}
-
-					{#each { length: paginator.pageRange } as _, index (index)}
-						<a
-							href="?page={index + paginator.pageMin}{technologiesParam}"
-							aria-current={index + paginator.pageMin === paginator.currentPage
-								? 'page'
-								: undefined}
-							aria-disabled="false"
-							class="projects__info__paginator__item"
-						>
-							{index + paginator.pageMin}
-						</a>
-					{/each}
-
-					{#if paginator.currentPage === paginator.pageCount}
-						<button aria-disabled="true" class="projects__info__paginator__item" disabled>
-							<i><Fa icon={faAngleRight} /></i>
-						</button>
-					{:else}
-						<a
-							href="?page={paginator.currentPage === paginator.pageCount
-								? paginator.pageCount
-								: paginator.currentPage + 1}{technologiesParam}"
-							aria-disabled="false"
-							class="projects__info__paginator__item"
-						>
-							<i><Fa icon={faAngleRight} /></i>
-						</a>
-					{/if}
-				</div>
+	<form
+		method="POST"
+		action="?/filterProjects"
+		use:enhance={() => {
+			checked = false;
+		}}
+	>
+		<input type="checkbox" name="technologies-toggle" id="technologies-toggle" bind:checked />
+		<label for="technologies-toggle">
+			{#if technologiesQuery}
+				Filter: {technologiesQuery.split(',').join(', ')}
+			{:else}
+				Filter Technologies
 			{/if}
-		</div>
-	{/key}
+		</label>
+		<select name="technologies" id="technologies" size={technologies.length} multiple>
+			{#each technologies as technology}
+				<option value={technology.name}>
+					<img src={technology.icon} alt="{technology.name}'s logo" />
+					{technology.name}</option
+				>
+			{/each}
+		</select>
+		<button type="submit" class="btn btn--invert">Filter</button>
+	</form>
+
+	{#if projects.length === 0}
+		<h2>No projects found with the filters provided.</h2>
+	{:else}
+		{#key $page.url}
+			<div class="projects__info">
+				<div
+					class="projects__info__grid"
+					in:fly={{ x: inX, duration: 300, delay: 300 }}
+					out:fly={{ x: outX, duration: 300 }}
+				>
+					{#each projects as project}
+						<ProjectDisplay {project} />
+					{/each}
+				</div>
+				{#if paginator.pageCount > 1}
+					<div class="projects__info__paginator">
+						{#if paginator.currentPage === 1}
+							<button aria-disabled="true" class="projects__info__paginator__item" disabled>
+								<i><Fa icon={faAngleLeft} /></i>
+							</button>
+						{:else}
+							<a
+								href="?page={paginator.currentPage === 1
+									? 1
+									: paginator.currentPage - 1}{technologiesParam}"
+								aria-disabled="false"
+								class="projects__info__paginator__item"
+							>
+								<i><Fa icon={faAngleLeft} /></i>
+							</a>
+						{/if}
+
+						{#each { length: paginator.pageRange } as _, index (index)}
+							<a
+								href="?page={index + paginator.pageMin}{technologiesParam}"
+								aria-current={index + paginator.pageMin === paginator.currentPage
+									? 'page'
+									: undefined}
+								aria-disabled="false"
+								class="projects__info__paginator__item"
+							>
+								{index + paginator.pageMin}
+							</a>
+						{/each}
+
+						{#if paginator.currentPage === paginator.pageCount}
+							<button aria-disabled="true" class="projects__info__paginator__item" disabled>
+								<i><Fa icon={faAngleRight} /></i>
+							</button>
+						{:else}
+							<a
+								href="?page={paginator.currentPage === paginator.pageCount
+									? paginator.pageCount
+									: paginator.currentPage + 1}{technologiesParam}"
+								aria-disabled="false"
+								class="projects__info__paginator__item"
+							>
+								<i><Fa icon={faAngleRight} /></i>
+							</a>
+						{/if}
+					</div>
+				{/if}
+			</div>
+		{/key}
+	{/if}
 </section>
 
 <style lang="scss">
