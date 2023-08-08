d4033f1d3aad497921ec565e3fd5c553c601d17c
Spimy <officialspyme@gmail.com>
2023-08-08 20:07:08 +0400

    👌 IMPROVE: Load skills from technologies list in database

--- src/routes/(portfolio)/+page.svelte
+++ src/routes/(portfolio)/+page.svelte
@@ -5,48 +5,17 @@
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
+	$: ({ age, experienceYears, projects, technologies } = data);
 
-	// Temporarily loaded on client side. Will be loaded on server side later from database.
-	interface SkillsList {
-		name: string;
-		src: string;
-	}
-
-	const skills: SkillsList[] = [
-		{ name: 'html', src: '/skills/html.png' },
-		{ name: 'css', src: '/skills/css.png' },
-		{ name: 'scss', src: '/skills/scss.png' },
-		{ name: 'js', src: '/skills/js.png' },
-		{ name: 'ts', src: '/skills/ts.png' },
-		{ name: 'node', src: '/skills/node.png' },
-		{ name: 'express', src: '/skills/express.png' },
-		{ name: 'svelte', src: '/skills/svelte.png' },
-		{ name: 'vue', src: '/skills/vue.png' },
-		{ name: 'nuxt', src: '/skills/nuxt.png' },
-		{ name: 'sqlite', src: '/skills/sqlite.png' },
-		{ name: 'discord', src: '/skills/discord.png' },
-		{ name: 'mongo', src: '/skills/mongo.png' },
-		{ name: 'mysql', src: '/skills/mysql.png' },
-		{ name: 'redis', src: '/skills/redis.png' },
-		{ name: 'java', src: '/skills/java.png' },
-		{ name: 'python', src: '/skills/python.png' },
-		{ name: 'django', src: '/skills/django.png' },
-		{ name: 'heroku', src: '/skills/heroku.png' },
-		{ name: 'figma', src: '/skills/figma.png' },
-		{ name: 'git', src: '/skills/git.png' },
-		{ name: 'ubuntu', src: '/skills/ubuntu.png' },
-		{ name: 'adobexd', src: '/skills/adobexd.png' },
-		{ name: 'photoshop', src: '/skills/photoshop.png' },
-		{ name: 'nginx', src: '/skills/nginx.png' },
-		{ name: 'certbot', src: '/skills/certbot.png' }
-	];
-
-	function chunk(array: SkillsList[], size: number): SkillsList[][] {
+	function chunk(array: Technology[], size: number): Technology[][] {
 		if (!array) return [];
 		const firstChunk = array.slice(0, size);
 		if (!firstChunk.length) return [array];
 		return [firstChunk].concat(chunk(array.slice(size, array.length), size));
 	}
+
+	const contactPath = pages.find((page) => page.id == 'contact')?.path;
+	const projectsPath = pages.find((page) => page.id == 'projects')?.path;
 </script>
 
 <svelte:head>
@@ -60,53 +29,59 @@
 		<h2>I'm Spimy</h2>
 		<p>
 			At least that's the username that I've grown quite fond of. My name is actually William and I
-			am {data.age} years old. Coding since 2016 (~{data.experienceYears} years of experience) and I
-			got into programming for trying to code a
+			am {age} years old. Coding since 2016 (~{experienceYears} years of experience) and I got into programming
+			for trying to code a
 			<a href="https://spigotmc.org" rel="external" target="_blank">Spigot plugin</a>
 			for
 			<a href="https://minecraft.net" rel="external" target="_blank">Minecraft</a>; making Java my
 			first programming language.
 		</p>
-		<a href={pages.find((page) => page.id === 'contact')?.path} class="hero__cta btn"
-			>Contact Me »</a
-		>
+		<a href={contactPath} class="hero__cta btn">Contact Me »</a>
 	</div>
 </section>
 
-<section class="skills">
-	<h1>Skills</h1>
-	<div class="skills__container">
-		<div class="skills__container__texture" />
-		<div class="skills__container__texture" />
-		<div class="skills__container__hex-grid">
-			{#each chunk(skills, 3) as skillsList, i (i)}
-				<div
-					class={i % 2 === 0 ? 'skills__container__hex-grid--a' : 'skills__container__hex-grid--b'}
-				>
-					{#each skillsList as skill, j (j)}
-						<div class="skills__container__hex-grid__hex">
-							<img src={skill.src} alt="{skill.name} logo" />
-						</div>
-					{/each}
-				</div>
-			{/each}
+{#if technologies.length > 0}
+	<section class="skills">
+		<h1>Skills</h1>
+		<div class="skills__container">
+			<div class="skills__container__texture" />
+			<div class="skills__container__texture" />
+			<div class="skills__container__hex-grid">
+				{#each chunk(technologies, 3) as skillsList, i (i)}
+					<div
+						class={i % 2 === 0
+							? 'skills__container__hex-grid--a'
+							: 'skills__container__hex-grid--b'}
+					>
+						{#each skillsList as skill, j (j)}
+							<div class="skills__container__hex-grid__hex">
+								<a href="{projectsPath}?pages=1&technologies={skill.name}">
+									<img src={skill.icon} alt="{skill.name} logo" />
+								</a>
+							</div>
+						{/each}
+					</div>
+				{/each}
+			</div>
+			<div class="skills__container__texture" />
+			<div class="skills__container__texture" />
 		</div>
-		<div class="skills__container__texture" />
-		<div class="skills__container__texture" />
-	</div>
-</section>
+	</section>
+{/if}
 
 <section class="projects">
 	<h1><span>Latest</span><br />Projects</h1>
 	<div class="projects__info">
-		<div class="projects__info__grid">
-			{#each data.projects as project}
-				<ProjectDisplay {project} />
-			{/each}
-		</div>
-		<a href={pages.find((page) => page.id === 'projects')?.path} class="projects__info__cta btn"
-			>View More
-		</a>
+		{#if projects.length > 0}
+			<div class="projects__info__grid">
+				{#each projects as project, index (index)}
+					<ProjectDisplay {project} />
+				{/each}
+			</div>
+		{:else}
+			<h2 class="projects__info__no-projects">No projects have been uploaded yet.</h2>
+		{/if}
+		<a href={projectsPath} class="projects__info__cta btn">View More </a>
 	</div>
 </section>
 
