46445857f94052eecd97593eb3776940987b13b0
Spimy <officialspyme@gmail.com>
2023-08-08 20:06:17 +0400

    👌 IMPROVE: Make fields unique

--- src/lib/server/database/models/technologies.model.ts
+++ src/lib/server/database/models/technologies.model.ts
@@ -3,8 +3,8 @@ import { Document, Schema, model } from 'mongoose';
 export interface ITechnology extends Document, Technology {}
 
 const technologiesSchema = new Schema<ITechnology>({
-	icon: { type: String, required: true },
-	name: { type: String, required: true }
+	icon: { type: String, unique: true, required: true },
+	name: { type: String, unique: true, required: true }
 });
 
 export const Technology = model<ITechnology>('technologies', technologiesSchema);
