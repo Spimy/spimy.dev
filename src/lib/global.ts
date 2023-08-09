3b469a5344eb7d8fe2cb022e03433a5a5f772bc5
Spimy <officialspyme@gmail.com>
2023-08-09 21:50:19 +0400

    📦 NEW: Add contact form schema

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -1,6 +1,14 @@
+import zod from 'zod';
+
 export const pages: Page[] = [
 	{ id: 'home', display: 'Home', path: '/', navbar: true },
 	{ id: 'projects', display: 'Projects', path: '/projects', navbar: true },
 	{ id: 'contact', display: 'Contact', path: '/contact', navbar: true },
 	{ id: 'sitemap', display: 'Sitemap', path: '/sitemap.xml', navbar: false }
 ];
+
+export const contactFormSchema = zod.object({
+	name: zod.string().min(2),
+	email: zod.string().email(),
+	message: zod.string().min(2)
+});
