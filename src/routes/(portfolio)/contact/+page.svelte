8e528f6e948018ccb0f0476afeb4548fa09351b5
Spimy <officialspyme@gmail.com>
2023-08-11 15:13:34 +0400

    👌 IMPROVE: Add icons to labels

--- src/routes/(portfolio)/contact/+page.svelte
+++ src/routes/(portfolio)/contact/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { contactFormSchema } from '$lib/global';
+	import { faEnvelope, faMessage, faUser } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
 	import { superForm } from 'sveltekit-superforms/client';
-	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
@@ -21,38 +22,46 @@
 		<h1>Get in Touch!</h1>
 		<p>I'll get back to you as soon as possible.</p>
 	</div>
-	<SuperDebug data={$form} />
+	<!-- <SuperDebug data={$form} /> -->
 	<div class="contact__content">
-		<form method="POST" action="?/submitContactForm" use:enhance>
-			<div class="contact__form-group">
-				<label for="name">Name</label>
-				<input type="text" name="name" id="name" bind:value={$form.name} {...$constraints.name} />
-				{#if $errors.name}<small>{$errors.name}</small>{/if}
+		<form class="contact__content__form" method="POST" action="?/submitContactForm" use:enhance>
+			<div class="contact__content__form__group">
+				<label for="name"><i><Fa icon={faUser} /></i></label>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					placeholder="Name"
+					bind:value={$form.name}
+					{...$constraints.name}
+				/>
 			</div>
-			<div class="contact__form-group">
-				<label for="email">Email</label>
+			{#if $errors.name}<small class="contact__content__form__error">{$errors.name}</small>{/if}
+			<div class="contact__content__form__group">
+				<label for="email"><i><Fa icon={faEnvelope} /></i></label>
 				<input
 					type="email"
 					name="email"
 					id="email"
+					placeholder="Email"
 					bind:value={$form.email}
 					{...$constraints.email}
 				/>
-				{#if $errors.email}<small>{$errors.email}</small>{/if}
 			</div>
-			<div class="contact__form-group">
-				<label for="message">Message</label>
+			{#if $errors.email}<small class="contact__content__form__error">{$errors.email}</small>{/if}
+			<div class="contact__content__form__group">
+				<label for="message"><i><Fa icon={faMessage} /></i></label>
 				<textarea
 					name="message"
 					id="message"
+					placeholder="Message"
 					bind:value={$form.message}
 					{...$constraints.message}
 				/>
-				{#if $errors.message}<small>{$errors.message}</small>{/if}
 			</div>
-			<div class="contact__form-group">
-				<button type="submit">Send</button>
-			</div>
+			{#if $errors.message}<small class="contact__content__form__error">{$errors.message}</small
+				>{/if}
+			<button type="submit" class="btn">Send</button>
 		</form>
 	</div>
 </section>
