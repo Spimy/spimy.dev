1e80657c96972843e49c818743310b98c24dc79d
Spimy <officialspyme@gmail.com>
2023-08-11 21:35:43 +0400

    👌 IMPROVE: Add verify hcaptcha in form action

--- src/routes/(portfolio)/contact/+page.server.ts
+++ src/routes/(portfolio)/contact/+page.server.ts
@@ -1,6 +1,15 @@
-import { EMAIL_HOST, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_TO, EMAIL_USER } from '$env/static/private';
+import {
+	EMAIL_HOST,
+	EMAIL_PASSWORD,
+	EMAIL_PORT,
+	EMAIL_TO,
+	EMAIL_USER,
+	HCAPTCHA_SECRET
+} from '$env/static/private';
+import { PUBLIC_HCAPTCHA_SITEKEY_CONTACT } from '$env/static/public';
 import { contactFormSchema } from '$lib/global';
 import { fail, type Actions } from '@sveltejs/kit';
+import { verify } from 'hcaptcha';
 import { createTransport } from 'nodemailer';
 import { superValidate } from 'sveltekit-superforms/server';
 import type { PageServerLoad } from '../$types';
@@ -26,10 +35,15 @@ export const load: PageServerLoad = async (event) => {
 export const actions: Actions = {
 	submitContactForm: async (event) => {
 		const form = await superValidate(event, contactFormSchema);
+		if (!form.valid) return fail(400, { form });
 
-		if (!form.valid) {
-			return fail(400, { form });
-		}
+		const response = await verify(
+			HCAPTCHA_SECRET,
+			form.data.token,
+			event.getClientAddress(),
+			PUBLIC_HCAPTCHA_SITEKEY_CONTACT
+		);
+		if (!response.success) return fail(400, { form });
 
 		await transporter.sendMail({
 			to: EMAIL_TO,
@@ -37,6 +51,7 @@ export const actions: Actions = {
 			from: `"Spimy - Automated <${EMAIL_USER}>`,
 			subject: `Contact Form filled by ${form.data.name}`
 		});
+
 		return { form };
 	}
 };
