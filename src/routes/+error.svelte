066cf83e42166b2e88748250a20d519a64bace6d
Spimy <officialspyme@gmail.com>
2023-08-18 22:48:07 +0400

    📦 NEW: Add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="error">
+	<h1>{$page.status}</h1>
+	{#if $page.status == 404}
+		<p>Page not found</p>
+	{:else}
+		<p>
+			{$page.error?.message === `Error: ${$page.status}`
+				? 'Something went wrong'
+				: $page.error?.message}
+		</p>
+	{/if}
+	<a href="/" class="btn">Go back home</a>
+</section>
+
+<style lang="scss">
+	@use '../scss/layout/error';
+</style>
